d1557a0a24553ae1e5b13685026ceafa9eee7791
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2025-02-20 13:12:49 +0800

    ✨ 排除merge commits

--- Zsh/module/alias.zsh
+++ Zsh/module/alias.zsh
@@ -13,8 +13,8 @@ alias cz="pnpm czg"
 alias czw="pnpm -w czg"
 
 # git yesterday commits and copy to clipboard
-alias yw='git log --oneline --author="$(git config user.name)" --since=yesterday.midnight --until=midnight | pbcopy'
+alias yw='git log --oneline --no-merges  --author="$(git config user.name)" --since=yesterday.midnight --until=midnight | pbcopy'
 # today work
-alias tw='git log --oneline --author="$(git config user.name)" --since=midnight | pbcopy'
+alias tw='git log --oneline --no-merges  --author="$(git config user.name)" --since=midnight | pbcopy'
 
 echo "module alias.zsh loaded"
