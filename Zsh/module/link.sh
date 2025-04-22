bf08f5c9c0cb569f53098bdaf00c2df9cdd13887
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2025-04-22 11:35:32 +0800

    ✨ 添加wifi.zsh

--- Zsh/module/link.sh
+++ Zsh/module/link.sh
@@ -1,5 +1,11 @@
 # link all modules
 for module in ~/Code/config-set/Zsh/module/*.zsh; do
+    # check if the link already exists
+    if [ -L ~/.oh-my-zsh/custom/$(basename $module) ]; then
+        echo "link already exists, skipping $module"
+        continue
+    fi
+
     echo "linking $module"
     # link the module
     # https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
---
+++ Zsh/module/wifi.zsh
@@ -0,0 +1,3 @@
+alias wifi-wasu-net-dong="networksetup -setairportnetwork en0 "WasuNetDong" "zjgyrj111222""
+alias wifi-bgw-dong="networksetup -setairportnetwork en0 "GY-BGWDong" "zjgyrj111222""
+echo "module wifi.zsh loaded"
