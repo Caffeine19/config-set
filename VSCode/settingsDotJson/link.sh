cb0c6b805c1a20d17a4bd195d252e9844e77a924
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2025-05-10 23:15:16 +0800

    ✨ add link.sh

---
+++ VSCode/settingsDotJson/link.sh
@@ -0,0 +1,3 @@
+rm ~/Library/Application\ Support/Code/User/settings.json
+
+ln -s ~/Code/config-set/VSCode/settingsDotJson/build/settings.json ~/Library/Application\ Support/Code/User/settings.json
