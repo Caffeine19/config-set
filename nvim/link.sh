f9ca0cf17b25f6652ad22fba0b37499359fd692a
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2025-05-27 10:12:35 +0800

    🐛 fix nvim ln

--- nvim/link.sh
+++ nvim/link.sh
@@ -1,4 +1,4 @@
 # link idea vim
 ln -s ~/Code/config-set/nvim/.ideavimrc ~/.ideavimrc
 # link neo vim
-ln -s ~/Code/config-set/nvim/init.lua ~/.config/nvim/init.lua
+ln -s ~/Code/config-set/nvim ~/.config/nvim
