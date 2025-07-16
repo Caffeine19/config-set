4723b729940e4023ff251eeba5775a00e90070d5
UnnecTrycat <108726581+Caffeine19@users.noreply.github.com>
2025-07-16 20:18:43 +0800

    Delete Zsh/module/wifi.zsh

--- Zsh/module/wifi.zsh
+++
@@ -1,3 +0,0 @@
-alias wifi-wasu-net-dong="networksetup -setairportnetwork en0 "WasuNetDong" "zjgyrj111222""
-alias wifi-bgw-dong="networksetup -setairportnetwork en0 "GY-BGWDong" "zjgyrj111222""
-echo "[oh-my-zsh] module wifi.zsh loaded"
