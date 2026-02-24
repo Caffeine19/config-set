3881ead26ef7a574446a8d3148ab714372036f57
UnnecTrycat <108726581+Caffeine19@users.noreply.github.com>
2026-02-24 10:43:12 +0800

    :recycle: remove restart mac in warp.zsh

--- Zsh/module/warp.zsh
+++ Zsh/module/warp.zsh
@@ -1,6 +1,7 @@
 delete_warp_icon() {
     # this would only take effect after restart
-    rm -rf /Applications/Warp.app/Contents/PlugIns/WarpDockTilePlugin.docktileplugin && osascript -e 'tell application "System Events" to restart'
+    rm -rf /Applications/Warp.app/Contents/PlugIns/WarpDockTilePlugin.docktileplugin
+    echo "Warp icon deleted. Please restart your terminal to see the effect."
 }
 
 echo "[oh-my-zsh] module warp.zsh loaded"
